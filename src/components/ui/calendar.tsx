8957d30fdab246989702338fd883f6ea6cd79e9d
rupeshdeotale <rupeshdeotale@gmail.com>
2026-02-05 16:56:39 +0000

    Updated Calender

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -2,10 +2,18 @@
 
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker } from "react-day-picker"
+import { DayPicker, DropdownProps } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
@@ -22,8 +30,11 @@ function Calendar({
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
-        caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
+        caption:
+          "flex justify-center pt-1 relative items-center h-11",
+        caption_label:
+          "text-sm font-medium flex items-center gap-2",
+        caption_dropdowns: "flex gap-2",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
@@ -54,12 +65,51 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-        ),
+        Dropdown: (
+          props: DropdownProps 
+        ) => {
+          const [selectedValue, setSelectedValue] = React.useState<string>(String(props.value));
+
+          const handleValueChange = (newValue: string) => {
+            const newDate = new Date();
+            if (props.name === "months") {
+              newDate.setMonth(parseInt(newValue, 10));
+              const month = props.options.find(option => String(option.props.value) === newValue);
+              month && props.onChange && props.onChange(month.props.value as unknown as React.ChangeEvent<HTMLSelectElement>);
+            } else if (props.name === "years") {
+              newDate.setFullYear(parseInt(newValue, 10));
+              const year = props.options.find(option => String(option.props.value) === newValue);
+              year && props.onChange && props.onChange(year.props.value as unknown as React.ChangeEvent<HTMLSelectElement>)
+            }
+            setSelectedValue(newValue);
+          };
+
+          return (
+            <Select
+              value={selectedValue}
+              onValueChange={handleValueChange}
+            >
+              <SelectTrigger className={cn(buttonVariants({ variant: "ghost" }), "h-auto font-semibold")}>
+                <SelectValue>{props.caption}</SelectValue>
+              </SelectTrigger>
+              <SelectContent>
+                <ScrollArea className="h-80">
+                  {props.children &&
+                    React.Children.map(props.children, (child) => (
+                      <SelectItem
+                        value={String((child as React.ReactElement).props.value)}
+                        className="min-w-[var(--radix-select-trigger-width)]"
+                      >
+                        {(child as React.ReactElement).props.children}
+                      </SelectItem>
+                    ))}
+                </ScrollArea>
+              </SelectContent>
+            </Select>
+          )
+        },
+        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
